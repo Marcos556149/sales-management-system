74f0af2f8350aa462560233977c533efac88cb99
Marcos556149 <holsasas7@gmail.com>
2026-03-25 22:33:28 -0300

    setup: initialize Spring Boot project with basic dependencies

---
+++ sales_management_system/src/test/java/com/marcoscornejos/sales_management_system/SalesManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marcoscornejos.sales_management_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalesManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
