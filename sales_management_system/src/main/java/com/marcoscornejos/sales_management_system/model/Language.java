5229aacb35cc2b5974b7bd917a3d11ac68a1c1ea
Marcos556149 <holsasas7@gmail.com>
2026-04-02 12:48:20 -0300

    feat(model): add Language enum for system configuration

---
+++ sales_management_system/src/main/java/com/marcoscornejos/sales_management_system/model/Language.java
@@ -0,0 +1,11 @@
+package com.marcoscornejos.sales_management_system.model;
+
+/**
+ * Supported interface languages of the system.
+ */
+public enum Language {
+
+    EN, // English
+    ES  // Spanish
+
+}
