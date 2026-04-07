b4b6c209c2d924996f006c178fa541a5575f5fa7
Marcos556149 <holsasas7@gmail.com>
2026-04-07 16:20:00 -0300

    feat: add displayName to Language enum for UI representation

--- sales_management_system/src/main/java/com/marcoscornejos/sales_management_system/model/Language.java
+++ sales_management_system/src/main/java/com/marcoscornejos/sales_management_system/model/Language.java
@@ -1,12 +1,27 @@
 /**
- * Supported interface languages of the system.
+ * Represents the supported interface languages of the system.
+ *
+ * <p>This enum is used to define the available languages that can be selected
+ * by the user for the application's interface.</p>
+ *
+ * <p>Each language includes a human-readable display name, which can be used
+ * in the UI (e.g., dropdowns, labels).</p>
  */
 
 package com.marcoscornejos.sales_management_system.model;
 
+import lombok.Getter;
+
+@Getter
 public enum Language {
 
-    EN, // English
-    ES  // Spanish
+    EN("English"),
+    ES("Spanish");
+
+    private final String displayName;
+
+    Language(String displayName) {
+        this.displayName = displayName;
+    }
 
 }
